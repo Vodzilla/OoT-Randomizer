09d0d5616ba2bcfd131a46f891fdcfbd21eb7395
Rob Realmuto <rrealmuto@gmail.com>
2022-02-14 22:01:56 -0500

    Testing a clean freestanding/pot shuffle merge

--- ASM/src/build.asm
+++ ASM/src/build.asm
@@ -100,6 +100,9 @@ RANDO_CONTEXT:
 .include "medigoron.asm"
 .include "misc_colors.asm"
 .include "door_of_time_col_fix.asm"
+.include "save.asm"
+.include "obj_mure3.asm"
+.include "bg_haka_tubo.asm"
 
 .align 0x10
 .importobj "../build/bundle.o"
